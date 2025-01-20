4c0ea5e62524675a190811b4ba80fe994e4b75ae
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-01-20 14:38:34 -0500

    Added test names

--- zippy/utils_test.go
+++ zippy/utils_test.go
@@ -6,18 +6,19 @@ import (
 
 func TestValidateCopy(t *testing.T) {
 	tests := []struct {
+		testName string
 		filePath string
 		written  int64
 		expected int64
 		wantErr  bool
 	}{
-		{"/invalid/path\0001", 100, 100, true},
-		{"/valid/path", 100, 200, true},
-		{"/valid/path", 100, 100, false},
+		{"Invalid Path", "/invalid/path\0001", 100, 100, true},
+		{"Mismatched Bytes", "/valid/path", 100, 200, true},
+		{"Valid Copy", "/valid/path", 100, 100, false},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.filePath, func(t *testing.T) {
+		t.Run(tt.testName, func(t *testing.T) {
 			err := validateCopy(tt.filePath, tt.written, tt.expected)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("validateCopy() error = %v, wantErr %v", err, tt.wantErr)
