c1800a692f2c462141b4eec9fae6bdc2874b8102
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-01-21 20:17:00 -0500

    Refactored

--- main.go
+++ main.go
@@ -6,31 +6,8 @@ import (
 	"path/filepath"
 
 	"example.com/m/zippy"
+	"example.com/m/zippy/testutils"
 )
-
-func createTempFile(dir, name string) error {
-	tempFile, err := os.CreateTemp(dir, name)
-	if err != nil {
-		return err
-	}
-	defer tempFile.Close()
-
-	_, err = tempFile.Write([]byte(filepath.Base(tempFile.Name())))
-	if err != nil {
-		panic(err)
-	}
-
-	return nil
-}
-
-func createTestFiles(dir string, files int) error {
-	for i := 0; i < files; i++ {
-		if err := createTempFile(dir, fmt.Sprintf("test%d-*.txt", i)); err != nil {
-			return err
-		}
-	}
-	return nil
-}
 
 func main() {
 	tempDir, err := os.MkdirTemp("", "test-")
@@ -39,35 +16,32 @@ func main() {
 	}
 	defer os.RemoveAll(tempDir)
 
-	if err := createTestFiles(tempDir, 10); err != nil {
+	baseDir := ".test"
+	tgtDir := filepath.Join(baseDir, "test2")
+	zipFile := filepath.Join(baseDir, "test", "test.zip")
+
+	os.RemoveAll(".test")
+
+	if err := testutils.CreateTestFiles(tempDir, 3, 2); err != nil {
 		panic(err)
 	}
 
-	if err := zippy.Zip(tempDir, "test/test.zip"); err != nil {
+	if err := zippy.Zip(tempDir, zipFile); err != nil {
 		panic(err)
 	}
 
-	if err := zippy.Unzip("test/test.zip", "test2"); err != nil {
+	zFiles, err := zippy.Contents(zipFile)
+	if err != nil {
 		panic(err)
 	}
 
-	// err = zippy.Zip("test2", "test3/test2-bak.zip")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	fmt.Printf("Contents of zip file (%s):\n\n", zipFile)
 
-	// err = zippy.Zip("test/test1", "test3/testfile1.zip")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	for _, zFile := range zFiles {
+		fmt.Printf("    %s\n", zFile.Name)
+	}
 
-	// err = zippy.Zip("test/test2", "test3/testfile2.zip")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// err = zippy.Zip("test", "test3/test-dir.zip")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	if err := zippy.Unzip(zipFile, tgtDir); err != nil {
+		panic(err)
+	}
 }
