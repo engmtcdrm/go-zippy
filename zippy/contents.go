ab3aaab3cfc4df4ba96ce1e5a67a95a9e4d5d924
John <deedsoftheforsaken@gmail.com>
2025-01-26 19:56:34 -0500

    Added info on Contents func

--- zippy/contents.go
+++ zippy/contents.go
@@ -4,6 +4,9 @@ import (
 	"archive/zip"
 )
 
+// Contents returns a list of files in the zip archive.
+//
+// zipFile is the path to the zip archive.
 func Contents(zipFile string) ([]*zip.File, error) {
 	zipRead, err := zip.OpenReader(zipFile)
 	if err != nil {
