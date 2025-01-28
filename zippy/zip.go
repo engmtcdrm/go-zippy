bd3746938e43694cf393547ada5e1138a895050b
John <deedsoftheforsaken@gmail.com>
2025-01-27 21:05:13 -0500

    Refactored Zip func to support multiple files Added AddToZip func

--- zippy/zip.go
+++ zippy/zip.go
@@ -8,6 +8,11 @@ import (
 	"path/filepath"
 )
 
+// zipFile adds a file or directory to a zip archive.
+//
+// path is the file or directory to add.
+//
+// zipWriter is the zip.Writer to write to.
 func zipFile(path string, zipWriter *zip.Writer, baseDir string) error {
 	relPath, err := filepath.Rel(baseDir, path)
 	if err != nil {
@@ -60,13 +65,38 @@ func zipFile(path string, zipWriter *zip.Writer, baseDir string) error {
 	return err
 }
 
-// Zip compresses a file or directory to a zip archive.
+// zipFiles adds files or directories to a zip archive.
+//
+// zipWriter is the zip.Writer to write to.
+//
+// file is the file or directory to add.
+func zipFiles(zipWriter *zip.Writer, file string) error {
+	fInfo, err := os.Stat(file)
+	if err != nil {
+		return err
+	}
+
+	if fInfo.IsDir() {
+		err = filepath.WalkDir(file, func(path string, entry os.DirEntry, walkErr error) error {
+			if walkErr != nil {
+				return walkErr
+			}
+			return zipFile(path, zipWriter, file)
+		})
+	} else {
+		err = zipFile(file, zipWriter, filepath.Dir(file))
+	}
+
+	return err
+}
+
+// Zip compresses multiple files or directories to a zip archive.
 // The destination file will be created if it does not exist.
 //
-// file is the file or directory to compress.
-//
 // dest is the destination zip archive path.
-func Zip(file string, dest string) error {
+//
+// files are the files or directories to compress.
+func Zip(dest string, files ...string) error {
 	// Make sure the destination directory exists
 	if err := os.MkdirAll(filepath.Dir(dest), os.ModePerm); err != nil {
 		return err
@@ -89,21 +119,78 @@ func Zip(file string, dest string) error {
 		}
 	}()
 
-	fInfo, err := os.Stat(file)
-	if err != nil {
+	for _, file := range files {
+		if err := zipFiles(zipWriter, file); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// AddToZip adds multiple files or directories to an existing zip archive.
+// The destination file will be created if it does not exist.
+//
+// dest is the destination zip archive path.
+//
+// files are the files or directories to compress.
+func AddToZip(dest string, files ...string) error {
+	// Make sure the destination directory exists
+	if err := os.MkdirAll(filepath.Dir(dest), os.ModePerm); err != nil {
 		return err
 	}
 
-	if fInfo.IsDir() {
-		err = filepath.WalkDir(file, func(path string, entry os.DirEntry, walkErr error) error {
-			if walkErr != nil {
-				return walkErr
+	destZipFile, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if closeErr := destZipFile.Close(); closeErr != nil {
+			err = fmt.Errorf("failed to close zip file: %w", closeErr)
+		}
+	}()
+
+	zipReader, err := zip.OpenReader(dest)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	defer func() {
+		if closeErr := zipReader.Close(); closeErr != nil {
+			err = fmt.Errorf("failed to close zip reader: %w", closeErr)
+		}
+	}()
+
+	zipWriter := zip.NewWriter(destZipFile)
+	defer func() {
+		if closeErr := zipWriter.Close(); closeErr != nil {
+			err = fmt.Errorf("failed to close zip writer: %w", closeErr)
+		}
+	}()
+
+	// Copy existing files to the new zip archive if zip file exists
+	if err == nil {
+		for _, f := range zipReader.File {
+			w, err := zipWriter.CreateHeader(&f.FileHeader)
+			if err != nil {
+				return err
 			}
-			return zipFile(path, zipWriter, file)
-		})
-	} else {
-		err = zipFile(file, zipWriter, filepath.Dir(file))
+			rc, err := f.Open()
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(w, rc)
+			rc.Close()
+			if err != nil {
+				return err
+			}
+		}
 	}
 
-	return err
+	for _, file := range files {
+		if err := zipFiles(zipWriter, file); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
