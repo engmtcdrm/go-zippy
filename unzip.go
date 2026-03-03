77a6bab4afa98fb11eeefd96bf516a30e0940315
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-03-03 01:04:48 +0000

    refactor: Reduce cognitive complexity

--- unzip.go
+++ unzip.go
@@ -27,8 +27,6 @@ type Unzippy struct {
 }
 
 // NewUnzippy creates a new Unzippy instance.
-//
-// path is the path to the zip archive.
 func NewUnzippy(path string, options *UnzippyOptions) *Unzippy {
 	if options == nil {
 		options = &UnzippyOptions{}
@@ -39,63 +37,6 @@ func NewUnzippy(path string, options *UnzippyOptions) *Unzippy {
 	}
 }
 
-// unzipFile extracts a single file from a zip archive.
-// The file is extracted to the specified path. The file
-// is validated using the CRC32 checksum and the size
-// of the extracted file is validated against the expected
-// size.
-//
-// file is the file to extract.
-//
-// filePath is the path to extract the file to.
-func (u *Unzippy) unzipFile(file *zip.File, filePath string) error {
-	zippedFile, err := file.Open()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if closeErr := zippedFile.Close(); closeErr != nil {
-			err = fmt.Errorf("failed to close zipped file: %w", closeErr)
-		}
-	}()
-
-	// Ensure the directory for the inflated file exists
-	fileDir := filepath.Dir(filePath)
-	if err := os.MkdirAll(fileDir, os.ModePerm); err != nil {
-		return err
-	}
-
-	outFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if closeErr := outFile.Close(); closeErr != nil {
-			err = fmt.Errorf("failed to close file: %w", closeErr)
-		}
-	}()
-
-	hash := crc32.NewIEEE()
-
-	// Copy the zipped file to the output file and calculate the checksum
-	// using a TeeReader to read from the zipped file and write to the hash
-	// at the same time.
-	written, err := io.Copy(outFile, io.TeeReader(zippedFile, hash))
-	if err != nil {
-		return err
-	}
-
-	checksum := hash.Sum32()
-
-	if checksum != file.CRC32 {
-		return fmt.Errorf("failed to copy '%s': expected '%08x' checksum, got '%08x' checksum", filePath, file.CRC32, checksum)
-	}
-
-	err = validateCopy(filePath, written, int64(file.UncompressedSize64))
-
-	return err
-}
-
 // Extract all files from zip archive to the same directory as the archive.
 func (u *Unzippy) Extract() ([]*zip.File, error) {
 	return u.ExtractFiles()
@@ -104,8 +45,6 @@ func (u *Unzippy) Extract() ([]*zip.File, error) {
 // Extracts all files from the zip archive to a destination directory.
 // The destination directory will be created if it does not exist.
 // The file modification times will be preserved.
-//
-// dest is the destination directory.
 func (u *Unzippy) ExtractTo(dest string) ([]*zip.File, error) {
 	return u.ExtractFilesTo(dest)
 }
@@ -127,9 +66,7 @@ func (u *Unzippy) ExtractFiles(files ...string) ([]*zip.File, error) {
 // files to be extracted. If no files are specified, all files will be extracted.
 // Glob patterns are supported.
 func (u *Unzippy) ExtractFilesTo(dest string, files ...string) ([]*zip.File, error) {
-	var err error
-
-	if err = os.MkdirAll(dest, os.ModePerm); err != nil {
+	if err := os.MkdirAll(dest, os.ModePerm); err != nil {
 		return nil, err
 	}
 
@@ -137,32 +74,50 @@ func (u *Unzippy) ExtractFilesTo(dest string, files ...string) ([]*zip.File, err
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
-		if closeErr := zipReader.Close(); closeErr != nil {
-			err = fmt.Errorf("failed to close zip file: %w", closeErr)
-		}
-	}()
+	defer zipReader.Close()
 
 	extFiles := zipReader.File
 
-	// If we have files to extract, filter the files to extract
-	if files != nil {
-		extFiles = []*zip.File{}
-		for _, file := range zipReader.File {
-			for _, f := range files {
-				match, err := filepath.Match(f, file.Name)
-				if err != nil {
-					return nil, err
-				}
+	extFiles, err = u.filterFiles(zipReader, files...)
+	if err != nil {
+		return nil, err
+	}
 
-				if match {
-					extFiles = append(extFiles, file)
-				}
+	if err := u.unzipFiles(dest, extFiles...); err != nil {
+		return nil, err
+	}
+
+	return extFiles, nil
+}
+
+// filterFiles filters the files in the zip archive based on the specified files to extract.
+func (u *Unzippy) filterFiles(zipReader *zip.ReadCloser, files ...string) ([]*zip.File, error) {
+	// If we have files to extract, filter the files to extract
+	if files == nil {
+		return zipReader.File, nil
+	}
+
+	extFiles := []*zip.File{}
+	for _, file := range zipReader.File {
+		for _, f := range files {
+			match, err := filepath.Match(f, file.Name)
+			if err != nil {
+				return nil, err
+			}
+
+			if match {
+				extFiles = append(extFiles, file)
 			}
 		}
 	}
 
-	for _, file := range extFiles {
+	return extFiles, nil
+}
+
+// unzipFiles extracts the specified files from the zip archive to a
+// destination directory.
+func (u *Unzippy) unzipFiles(dest string, files ...*zip.File) error {
+	for _, file := range files {
 		if u.Options.Junk {
 			file.Name = filepath.Base(file.Name)
 		}
@@ -171,19 +126,59 @@ func (u *Unzippy) ExtractFilesTo(dest string, files ...string) ([]*zip.File, err
 
 		if file.FileInfo().IsDir() {
 			if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
-				return nil, err
+				return err
 			}
 		} else {
 			if err := u.unzipFile(file, filePath); err != nil {
-				return nil, err
+				return err
 			}
 		}
 
 		// Preserve the file modification date
 		if err := os.Chtimes(filePath, file.Modified, file.Modified); err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	return extFiles, err
+	return nil
+}
+
+// unzipFile extracts a single file from a zip archive.
+func (u *Unzippy) unzipFile(zipFile *zip.File, dest string) error {
+	zippedFile, err := zipFile.Open()
+	if err != nil {
+		return err
+	}
+	defer zippedFile.Close()
+
+	// Ensure the directory for the inflated file exists
+	fileDir := filepath.Dir(dest)
+	if err := os.MkdirAll(fileDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	outFile, err := os.OpenFile(dest, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, zipFile.Mode())
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	hash := crc32.NewIEEE()
+
+	// Copy the zipped file to the output file and calculate the checksum
+	// using a TeeReader to read from the zipped file and write to the hash
+	// at the same time.
+	written, err := io.Copy(outFile, io.TeeReader(zippedFile, hash))
+	if err != nil {
+		return err
+	}
+
+	checksum := hash.Sum32()
+
+	// Verify the checksum of the extracted file against the expected checksum from the zip file.
+	if checksum != zipFile.CRC32 {
+		return fmt.Errorf("failed to copy '%s': expected '%08x' checksum, got '%08x' checksum", dest, zipFile.CRC32, checksum)
+	}
+
+	return validateCopy(dest, written, int64(zipFile.UncompressedSize64))
 }
