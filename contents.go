00c8d5adca85d32427d20eed74764108654ebc6a
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-03-08 01:38:08 +0000

    chore: revised comment

--- contents.go
+++ contents.go
@@ -5,8 +5,6 @@ import (
 )
 
 // Contents returns a list of files in the zip archive.
-//
-// zipFile is the path to the zip archive.
 func Contents(zipFile string) ([]*zip.File, error) {
 	zipRead, err := zip.OpenReader(zipFile)
 	if err != nil {
