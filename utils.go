4896e305f32dca0e0a69638e6eaa02a829abbfc4
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-03-08 01:10:35 +0000

    chore: set functions alphabetically

--- utils.go
+++ utils.go
@@ -7,32 +7,6 @@ import (
 	"strings"
 )
 
-// Validates the number of bytes written during a copy operation with
-// expected number of bytes.
-//
-// path is the path of the file that was copied.
-//
-// written is the number of bytes written.
-//
-// expected is the expected number of bytes.
-func validateCopy(path string, written int64, expected int64) error {
-	absPath, err := filepath.Abs(path)
-	if err != nil {
-		return err
-	}
-
-	_, err = os.Stat(path)
-	if err != nil {
-		return err
-	}
-
-	if written != expected {
-		return fmt.Errorf("failed to copy '%s': expected %d bytes, got %d bytes", absPath, expected, written)
-	}
-
-	return nil
-}
-
 // Removes the drive letter and colon from a Windows path.
 func removeDriveLetter(path string) string {
 	return strings.TrimPrefix(path, filepath.VolumeName(path))
@@ -51,4 +25,24 @@ func toZipPaths(paths ...string) []string {
 	}
 
 	return paths
+}
+
+// Validates the number of bytes written during a copy operation with
+// expected number of bytes.
+func validateCopy(path string, written int64, expected int64) error {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if written != expected {
+		return fmt.Errorf("failed to copy '%s': expected %d bytes, got %d bytes", absPath, expected, written)
+	}
+
+	return nil
 }
