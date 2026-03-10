c031efbeb0d94a9f8c10c1a08dfee82cb2758def
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-03-10 01:54:26 +0000

    refactor: contents example

--- examples/internal/all_examples.go
+++ examples/internal/all_examples.go
@@ -1,5 +1,9 @@
 package internal
 
-import "github.com/engmtcdrm/go-eggy"
+import (
+	"github.com/engmtcdrm/go-eggy"
+)
 
-var AllExamples = []eggy.Example{}
+var AllExamples = []eggy.Example{
+	{Name: "Contents Example", Fn: ExampleContents},
+}
