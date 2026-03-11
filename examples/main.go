7e6b9aa96741668336790982219d9c00ec49b957
John <deedsoftheforsaken@gmail.com>
2026-03-11 01:51:01 +0000

    chore: added slog handler

--- examples/main.go
+++ examples/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log/slog"
+
 	"github.com/engmtcdrm/go-eggy"
+	"github.com/engmtcdrm/go-lager"
 	pp "github.com/engmtcdrm/go-prettyprint"
 	"github.com/engmtcdrm/go-zippy/examples/internal"
 )
 
 func main() {
+	opts := &lager.HandlerOptions{Level: lager.LevelDebug}
+	sout := lager.NewStreamHandler(lager.StreamStdout, opts)
+	logger := slog.New(sout)
+	slog.SetDefault(logger)
+
 	ex := eggy.NewExamplePrompt(internal.AllExamples).
 		Title(pp.Yellow("Examples of Zippy"))
 	ex.Show()
